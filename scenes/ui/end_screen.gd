2a20d5d4644f91dcbd1581711cf65bc628606d24
Kirill Klimkin <violinkk29@gmail.com>
2024-01-25 19:19:50 +0300

    finished playable demo

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -1,8 +1,8 @@
 extends Control
 
 func _ready():
-	$Score.text = str(Globals.score)
-	
-func _on_retry_pressed():
+	$VBoxContainer/Score.text = str(Globals.score)
+
+func _on_texture_button_pressed():
 		Globals.score = 0
 		get_tree().change_scene_to_file("res://scenes/lvls/lvl.tscn")
